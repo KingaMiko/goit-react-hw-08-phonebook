f0e161858b24dc5144add9a19e52e9854eee8e2a
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-09-29 18:50:44 +0200

    styled AppBar ane HomePahe

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { Helmet } from 'react-helmet';
+import PhoneIcon from '@mui/icons-material/Phone';
 
 export default function HomePage() {
   return (
@@ -12,28 +13,59 @@ export default function HomePage() {
         display="flex"
         flexDirection="column"
         alignItems="center"
-        justifyContent="flex-start"
-        borderRadius="5px"
-        maxWidth="500px"
-        width="auto"
-        minHeight="500px"
-        margin="50px auto"
-        bgcolor="rgba(232, 160, 191, 0.7)"
-        boxShadow="0 9px 50px hsla(20, 67%, 75%, 0.31)"
-        sx={{
-          '@media screen and (max-width: 767px)': {
-            width: '100%',
-            height: '100vh',
-          },
-        }}
+        justifyContent="center"
+        height="100vh"
+        background="linear-gradient(45deg, #f2f2f2 30%, #d9d9d9 90%)"
+        overflow="hidden"
+        position="relative"
       >
+        <Box
+          position="absolute"
+          top={0}
+          left={0}
+          width="100%"
+          height="100%"
+          zIndex="-1"
+          overflow="hidden"
+        >
+          <PhoneIcon
+            style={{
+              fontSize: '20vw',
+              opacity: 0.1,
+              position: 'absolute',
+              top: '20%',
+              left: '10%',
+              transform: 'rotate(25deg)',
+            }}
+          />
+          <PhoneIcon
+            style={{
+              fontSize: '20vw',
+              opacity: 0.1,
+              position: 'absolute',
+              bottom: '20%',
+              right: '10%',
+              transform: 'rotate(-25deg)',
+            }}
+          />
+        </Box>
+
         <Typography
-          variant="h4"
+          variant="h2"
           component="h1"
           textAlign="center"
-          margin="30px"
+          color="rgba(0,0,0,0.7)"
+          fontWeight="bold"
+          mb={3}
         >
           Welcome to Phonebook!
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          color="rgba(0,0,0,0.5)"
+          textAlign="center"
+        >
+          Your digital directory for contacts.
         </Typography>
       </Box>
     </>
