678238c49057dca6b1749425ce99057e865c713d
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2024-04-23 13:09:30 +0200

    bad words validation in useForm hook

--- src/components/hooks/useForm.js
+++ src/components/hooks/useForm.js
@@ -1,4 +1,193 @@
 import { useState } from 'react';
+import Filter from 'bad-words';
+
+const customList = [
+  // Odmiany słowa "kurwa"
+  'kurwa',
+  'kurwie',
+  'kurwą',
+  'kurwę',
+  'kurwo',
+  'kurwy',
+  'kurwach',
+  'kurwami',
+  'kurwiska',
+  'kurwiszcze',
+  'kurwiszon',
+  'kurwiszony',
+  'kurwiszonów',
+  'kurwojady',
+  'kurwojadów',
+
+  // Odmiany słowa "chuj"
+  'chuj',
+  'chuja',
+  'chujem',
+  'chuju',
+  'chujowi',
+  'chujów',
+  'chuje',
+  'chujek',
+  'chujami',
+  'chujoza',
+  'chujnia',
+  'chujowy',
+  'chujowa',
+  'chujowe',
+
+  // Odmiany słowa "pierdolić"
+  'pierdolę',
+  'pierdolić',
+  'pierdolisz',
+  'pierdoli',
+  'pierdolą',
+  'pierdolony',
+  'pierdolona',
+  'pierdolone',
+  'pierdolęcie',
+  'pierdolenie',
+  'pierdolnięty',
+  'pierdolnięta',
+  'pierdolnięte',
+  'spierdolić',
+  'spierdolił',
+  'opierdolić',
+  'opierdol',
+  'opierdoli',
+  'opierdala',
+
+  // Odmiany słowa "jebać"
+  'jebać',
+  'jeb',
+  'jebie',
+  'jebię',
+  'jebiesz',
+  'jebią',
+  'jebany',
+  'jebana',
+  'jebane',
+  'jebanka',
+  'jebanko',
+  'wyjebać',
+  'wyjebie',
+  'wyjebią',
+  'wyjebiesz',
+  'wyjebali',
+  'zajebisty',
+  'zajebista',
+  'zajebiste',
+  'zajebać',
+  'zajebie',
+  'zajebią',
+  'zajebiesz',
+  'zajebali',
+  'zjebać',
+  'zjebie',
+  'zjebią',
+  'zjebiesz',
+  'zjebane',
+  'dojebać',
+  'dojebie',
+  'dojebią',
+  'dojebiesz',
+  'dojebane',
+
+  // Odmiany słowa "spierdalać"
+  'spierdalaj',
+  'spierdalać',
+  'spierdalam',
+  'spierdalają',
+  'spierdalał',
+  'spierdalała',
+  'spierdalajcie',
+  'spierdolić',
+  'spierdoli',
+  'spierdolą',
+  'spierdolił',
+  'spierdoliła',
+  'spierdoliło',
+  'spierdolisz',
+  'spierdolicie',
+  'pospierdalać',
+  'pospierdalaj',
+  'pospierdala',
+  'pospierdali',
+
+  // Odmiany słowa "pizda"
+  'pizda',
+  'pizdzie',
+  'pizdą',
+  'pizdu',
+  'pizdy',
+  'pizdom',
+  'pizdach',
+  'pizdami',
+
+  // Inne wulgaryzmy i ich odmiany
+  'cipa',
+  'cipie',
+  'cipą',
+  'cipę',
+  'cipy',
+  'cipach',
+  'cipami',
+  'dupa',
+  'dupie',
+  'dupą',
+  'dupę',
+  'dupy',
+  'dupach',
+  'dupami',
+  'huj',
+  'huja',
+  'hujowi',
+  'hujem',
+  'huju',
+  'hujów',
+  'huje',
+  'hujami',
+  'dziwka',
+  'dziwki',
+  'dziwce',
+  'dziwkę',
+  'dziwko',
+  'dziwek',
+  'dziwką',
+  'dziwkami',
+  'dziwkach',
+  'cipa',
+  'cipie',
+  'cipą',
+  'cipę',
+  'cipy',
+  'cipach',
+  'cipami',
+  'dupa',
+  'dupie',
+  'dupą',
+  'dupę',
+  'dupy',
+  'dupach',
+  'dupami',
+  'huj',
+  'huja',
+  'hujowi',
+  'hujem',
+  'huju',
+  'hujów',
+  'huje',
+  'hujami',
+
+  // Błędne wersje literowe popularnych wulgaryzmów
+  'kvrwa',
+  'chvuj',
+  'pierdvlę',
+  'jebvny',
+  'spierdvlaj',
+  'pizdva',
+];
+
+const filter = new Filter({ list: customList });
 
 export const useForm = initialValues => {
   const [values, setValues] = useState(initialValues);
@@ -38,22 +227,28 @@ export const useForm = initialValues => {
         delete newErrors[name];
         setErrors(newErrors);
       } else {
-        const letterCount = (value.match(/[a-zA-Z]/g) || []).length;
-        if (letterCount < 3 || value.length > 20) {
+        if (filter.isProfane(value)) {
           setErrors({
             ...errors,
-            [name]:
-              'Name must contain at least 3 letters and be no longer than 20 characters',
+            [name]: 'Please avoid using offensive language',
           });
         } else {
-          let newErrors = { ...errors };
-          delete newErrors[name];
-          setErrors(newErrors);
+          const letterCount = (value.match(/[a-zA-Z]/g) || []).length;
+          if (letterCount < 3 || value.length > 20) {
+            setErrors({
+              ...errors,
+              [name]:
+                'Name must contain at least 3 letters and be no longer than 20 characters',
+            });
+          } else {
+            let newErrors = { ...errors };
+            delete newErrors[name];
+            setErrors(newErrors);
+          }
         }
       }
     }
   };
-
   const resetForm = () => {
     setValues(initialValues);
     setErrors({});
@@ -64,10 +259,11 @@ export const useForm = initialValues => {
     if (
       !values.name ||
       (values.name.match(/[a-zA-Z]/g) || []).length < 3 ||
-      values.name.length > 20
+      values.name.length > 20 ||
+      filter.isProfane(values.name)
     ) {
       tempErrors.name =
-        'Name must contain at least 3 letters and be no longer than 20 characters';
+        'Name must contain at least 3 letters and be no longer than 20 characters and not contain offensive language';
     }
     if (
       !values.number.match(
